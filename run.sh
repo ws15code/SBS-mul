47acb0d89007efebef8013f9962bc34f5c8fc72d
Hao Tang <haotang@ttic.edu>
2015-07-22 17:36:23 +0000

    Add Urdu

--- run.sh
+++ run.sh
@@ -20,106 +20,107 @@ NUMLEAVES=1200
 NUMGAUSSIANS=8000
 
 # Set the language codes for SBS languages that we will be processing
-export SBS_LANGUAGES="MD SW AR"
-export TRAIN_LANG="SW AR"
+export SBS_LANGUAGES="MD SW AR UR"
+export TRAIN_LANG="SW AR UR"
 export TEST_LANG="MD"
 
-# #### LANGUAGE SPECIFIC SCRIPTS HERE ####
-# local/sbs_data_prep.sh --config-dir=$PWD/conf --corpus-dir=$SBS_CORPUS \
-#   --languages="$SBS_LANGUAGES"  --trans-dir=$SBS_TRANSCRIPTS --list-dir=$SBS_DATA_LISTS
+#### LANGUAGE SPECIFIC SCRIPTS HERE ####
+local/sbs_data_prep.sh --config-dir=$PWD/conf --corpus-dir=$SBS_CORPUS \
+  --languages="$SBS_LANGUAGES"  --trans-dir=$SBS_TRANSCRIPTS --list-dir=$SBS_DATA_LISTS
 
-# local/sbs_dict_prep.sh $SBS_LANGUAGES 2>&1 | tee data/prepare_dict.log
-# 
-# for L in $SBS_LANGUAGES; do
-#   echo "lang $L"
-#   utils/prepare_lang.sh --position-dependent-phones false \
-#     data/$L/local/dict "<unk>" data/$L/local/lang_tmp data/$L/lang \
-#     2>&1 | tee data/$L/prepare_lang.log || exit 1;
-# done
+echo "dict prep"
+local/sbs_dict_prep.sh $SBS_LANGUAGES >& data/prepare_dict.log
 
-# for L in $SBS_LANGUAGES; do
-#   echo "LM $L"
-#   local/sbs_format_phnlm.sh $L 2>&1 | tee data/$L/format_lm.log || exit 1;
-# done
-# wait
+for L in $SBS_LANGUAGES; do
+  echo "lang prep: $L"
+  utils/prepare_lang.sh --position-dependent-phones false \
+    data/$L/local/dict "<unk>" data/$L/local/lang_tmp data/$L/lang \
+    >& data/$L/prepare_lang.log || exit 1;
+done
 
-# # Make MFCC features.
-# for L in $SBS_LANGUAGES; do
-#   mfccdir=mfcc/$L
-#   for x in train eval; do
-#     (
-#       steps/make_mfcc.sh --nj 4 --cmd "$train_cmd" data/$L/$x \
-#         exp/$L/make_mfcc/$x $mfccdir;
-#       steps/compute_cmvn_stats.sh data/$L/$x exp/$L/make_mfcc/$x $mfccdir;
-#     ) &
-#   done
-# done
-# wait;
+for L in $SBS_LANGUAGES; do
+  echo "LM prep: $L"
+  local/sbs_format_phnlm.sh $L >& data/$L/format_lm.log || exit 1;
+done
+wait
 
-# local/sbs_uni_data_prep.sh "$TRAIN_LANG" "$TEST_LANG"
-# 
-# echo "universal lang"
-# utils/prepare_lang.sh --position-dependent-phones false \
-#   data/local/dict "<unk>" data/local/lang_tmp data/lang \
-#   2>&1 | tee data/prepare_lang.log || exit 1;
-# 
-# echo "universal LM"
-# local/sbs_format_uniphnlm.sh 2>&1 | tee data/format_lm.log || exit 1;
-# 
-# mfccdir=mfcc
-# # for x in train eval; do
+echo "MFCC prep"
+# Make MFCC features.
+for L in $SBS_LANGUAGES; do
+  mfccdir=mfcc/$L
+  for x in train eval; do
+    (
+      steps/make_mfcc.sh --nj 4 --cmd "$train_cmd" data/$L/$x \
+        exp/$L/make_mfcc/$x $mfccdir;
+      steps/compute_cmvn_stats.sh data/$L/$x exp/$L/make_mfcc/$x $mfccdir;
+    ) &
+  done
+done
+wait;
+
+local/sbs_uni_data_prep.sh "$TRAIN_LANG" "$TEST_LANG"
+
+echo "universal lang"
+utils/prepare_lang.sh --position-dependent-phones false \
+  data/local/dict "<unk>" data/local/lang_tmp data/lang \
+  2>&1 | tee data/prepare_lang.log || exit 1;
+
+echo "universal LM"
+local/sbs_format_uniphnlm.sh 2>&1 | tee data/format_lm.log || exit 1;
+
+mfccdir=mfcc
 # for x in train eval; do
-#   (
-#     steps/make_mfcc.sh --nj 4 --cmd "$train_cmd" data/$x exp/make_mfcc/$x $mfccdir;
-#     steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x $mfccdir;
-#   ) &
-# done
-# wait;
-# 
-# mkdir -p exp/mono;
-# steps/train_mono.sh --nj 8 --cmd "$train_cmd" \
-#   data/train data/lang exp/mono >& exp/mono/train.log
-# 
-# graph_dir=exp/mono/graph
-# mkdir -p $graph_dir
-# utils/mkgraph.sh --mono data/lang_test exp/mono \
-#   $graph_dir >& $graph_dir/mkgraph.log
-# for L in $SBS_LANGUAGES; do
-#   steps/decode.sh --nj 4 --cmd "$decode_cmd" $graph_dir data/$L/eval \
-#     exp/mono/decode_eval_$L
-# done
-# 
-# # Training/decoding triphone models
-# mkdir -p exp/mono_ali
-# steps/align_si.sh --nj 8 --cmd "$train_cmd" \
-#   data/train data/lang exp/mono exp/mono_ali \
-#   >& exp/mono_ali/align.log 
-# 
-# # Training triphone models with MFCC+deltas+double-deltas
-# mkdir -p exp/tri1
-# steps/train_deltas.sh --boost-silence 1.25 --cmd "$train_cmd" $NUMLEAVES $NUMGAUSSIANS \
-#   data/train data/lang exp/mono_ali exp/tri1 >& exp/tri1/train.log || exit 1;
-# 
-# graph_dir=exp/tri1/graph
-# mkdir -p $graph_dir
-# 
-# utils/mkgraph.sh data/lang_test exp/tri1 $graph_dir \
-#   >& $graph_dir/mkgraph.log 
-# 
-# for L in $SBS_LANGUAGES; do
-#   steps/decode.sh --nj 4 --cmd "$decode_cmd" $graph_dir data/$L/eval \
-#     exp/tri1/decode_eval_$L
-# done
+for x in train eval; do
+  (
+    steps/make_mfcc.sh --nj 4 --cmd "$train_cmd" data/$x exp/make_mfcc/$x $mfccdir;
+    steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x $mfccdir;
+  ) &
+done
+wait;
 
-# mkdir -p exp/tri1_ali
-# steps/align_si.sh --nj 8 --cmd "$train_cmd" \
-#   data/train data/lang exp/tri1 exp/tri1_ali \
-#   >& exp/tri1_ali/align.log 
-# 
-# mkdir -p exp/tri2a
-# steps/train_deltas.sh --cmd "$train_cmd" $NUMLEAVES $NUMGAUSSIANS \
-#   data/train data/lang exp/tri1_ali exp/tri2a || exit 1;
-# 
+mkdir -p exp/mono;
+steps/train_mono.sh --nj 8 --cmd "$train_cmd" \
+  data/train data/lang exp/mono >& exp/mono/train.log
+
+graph_dir=exp/mono/graph
+mkdir -p $graph_dir
+utils/mkgraph.sh --mono data/lang_test exp/mono \
+  $graph_dir >& $graph_dir/mkgraph.log
+for L in $SBS_LANGUAGES; do
+  steps/decode.sh --nj 4 --cmd "$decode_cmd" $graph_dir data/$L/eval \
+    exp/mono/decode_eval_$L
+done
+
+# Training/decoding triphone models
+mkdir -p exp/mono_ali
+steps/align_si.sh --nj 8 --cmd "$train_cmd" \
+  data/train data/lang exp/mono exp/mono_ali \
+  >& exp/mono_ali/align.log 
+
+# Training triphone models with MFCC+deltas+double-deltas
+mkdir -p exp/tri1
+steps/train_deltas.sh --boost-silence 1.25 --cmd "$train_cmd" $NUMLEAVES $NUMGAUSSIANS \
+  data/train data/lang exp/mono_ali exp/tri1 >& exp/tri1/train.log || exit 1;
+
+graph_dir=exp/tri1/graph
+mkdir -p $graph_dir
+
+utils/mkgraph.sh data/lang_test exp/tri1 $graph_dir \
+  >& $graph_dir/mkgraph.log 
+
+for L in $SBS_LANGUAGES; do
+  steps/decode.sh --nj 4 --cmd "$decode_cmd" $graph_dir data/$L/eval \
+    exp/tri1/decode_eval_$L
+done
+
+mkdir -p exp/tri1_ali
+steps/align_si.sh --nj 8 --cmd "$train_cmd" \
+  data/train data/lang exp/tri1 exp/tri1_ali \
+  >& exp/tri1_ali/align.log 
+
+mkdir -p exp/tri2a
+steps/train_deltas.sh --cmd "$train_cmd" $NUMLEAVES $NUMGAUSSIANS \
+  data/train data/lang exp/tri1_ali exp/tri2a || exit 1;
 
 graph_dir=exp/tri2a/graph
 mkdir -p $graph_dir
@@ -129,8 +130,9 @@ utils/mkgraph.sh data/lang_test exp/tri2a $graph_dir \
 
 for L in $SBS_LANGUAGES; do
   steps/decode.sh --nj 4 --cmd "$decode_cmd" $graph_dir data/$L/eval \
-    exp/tri2a/decode_eval_$L
+    exp/tri2a/decode_eval_$L &
 done
+wait
 
 mkdir -p exp/tri2b
 steps/train_lda_mllt.sh --cmd "$train_cmd" \
@@ -146,8 +148,9 @@ utils/mkgraph.sh data/lang_test exp/tri2b $graph_dir \
 
 for L in $SBS_LANGUAGES; do
   steps/decode.sh --nj 4 --cmd "$decode_cmd" $graph_dir data/$L/eval \
-    exp/tri2b/decode_eval_$L
+    exp/tri2b/decode_eval_$L &
 done
+wait
 
 mkdir -p exp/tri2b_ali
 
@@ -165,8 +168,9 @@ utils/mkgraph.sh data/lang_test exp/tri3b $graph_dir \
 
 for L in $SBS_LANGUAGES; do
   steps/decode_fmllr.sh --nj 4 --cmd "$decode_cmd" $graph_dir data/$L/eval \
-    exp/tri3b/decode_eval_$L
+    exp/tri3b/decode_eval_$L &
 done
+wait
 
 # Getting PER numbers
 # for x in exp/*/*/decode*; do [ -d $x ] && grep WER $x/wer_* | utils/best_wer.sh; done
