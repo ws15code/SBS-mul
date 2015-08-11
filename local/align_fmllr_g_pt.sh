05885f34caabb998115413a21165ffa19eb32c49
Chunxi Liu <cliu77@jhu.edu>
2015-08-11 07:37:33 +0000

    GMM adaptation with (bigram LM o PT) as G

---
+++ local/align_fmllr_g_pt.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+# Copyright 2012  Johns Hopkins University (Author: Daniel Povey)
+# Apache 2.0
+#
+# Adapted from steps/align_fmllr.sh
+
+# Computes training alignments; assumes features are (LDA+MLLT or delta+delta-delta)
+# + fMLLR (probably with SAT models).
+# It first computes an alignment with the final.alimdl (or the final.mdl if final.alimdl
+# is not present), then does 2 iterations of fMLLR estimation.
+
+# If you supply the --use-graphs option, it will use the training
+# graphs from the source directory (where the model is).  In this
+# case the number of jobs must match the source directory.
+
+
+# Begin configuration section.  
+stage=0
+nj=4
+cmd=run.pl
+use_graphs=false
+# Begin configuration.
+scale_opts="--transition-scale=1.0 --acoustic-scale=0.1 --self-loop-scale=0.1"
+beam=10
+#retry_beam=40
+retry_beam=100
+careful=false
+boost_silence=1.0 # factor by which to boost silence during alignment.
+fmllr_update_type=full
+# End configuration options.
+
+echo "$0 $@"  # Print the command line for logging
+
+[ -f path.sh ] && . ./path.sh # source the path.
+. parse_options.sh || exit 1;
+
+#if [ $# != 4 ]; then
+if [ $# != 5 ]; then
+   echo "usage: local/align_fmllr_g_pt.sh <data-dir> <lang-dir> <src-dir> <align-dir>"
+   echo "e.g.:  local/align_fmllr_g_pt.sh data/train data/lang exp/tri1 exp/tri1_ali"
+   echo "main options (for others, see top of script file)"
+   echo "  --config <config-file>                           # config containing options"
+   echo "  --nj <nj>                                        # number of parallel jobs"
+   echo "  --use-graphs true                                # use graphs in src-dir"
+   echo "  --cmd (utils/run.pl|utils/queue.pl <queue opts>) # how to run jobs."
+   echo "  --fmllr-update-type (full|diag|offset|none)      # default full."
+   exit 1;
+fi
+
+data=$1
+lang=$2
+srcdir=$3
+dir=$4
+dir_fsts=$5 # fsts of probabilistic transcriptions
+
+oov=`cat $lang/oov.int` || exit 1;
+silphonelist=`cat $lang/phones/silence.csl` || exit 1;
+sdata=$data/split$nj
+
+mkdir -p $dir/log
+echo $nj > $dir/num_jobs
+[[ -d $sdata && $data/feats.scp -ot $sdata ]] || split_data.sh $data $nj || exit 1;
+
+cp $srcdir/{tree,final.mdl} $dir || exit 1;
+cp $srcdir/final.alimdl $dir 2>/dev/null
+cp $srcdir/final.occs $dir;
+splice_opts=`cat $srcdir/splice_opts 2>/dev/null` # frame-splicing options.
+cp $srcdir/splice_opts $dir 2>/dev/null # frame-splicing options.
+cmvn_opts=`cat $srcdir/cmvn_opts 2>/dev/null`
+cp $srcdir/cmvn_opts $dir 2>/dev/null # cmn/cmvn option.
+delta_opts=`cat $srcdir/delta_opts 2>/dev/null`
+cp $srcdir/delta_opts $dir 2>/dev/null
+
+if [ -f $srcdir/final.mat ]; then feat_type=lda; else feat_type=delta; fi
+echo "$0: feature type is $feat_type"
+
+case $feat_type in
+  delta) sifeats="ark,s,cs:apply-cmvn $cmvn_opts --utt2spk=ark:$sdata/JOB/utt2spk scp:$sdata/JOB/cmvn.scp scp:$sdata/JOB/feats.scp ark:- | add-deltas $delta_opts ark:- ark:- |";;
+  lda) sifeats="ark,s,cs:apply-cmvn $cmvn_opts --utt2spk=ark:$sdata/JOB/utt2spk scp:$sdata/JOB/cmvn.scp scp:$sdata/JOB/feats.scp ark:- | splice-feats $splice_opts ark:- ark:- | transform-feats $srcdir/final.mat ark:- ark:- |"
+    cp $srcdir/final.mat $dir
+    cp $srcdir/full.mat $dir 2>/dev/null
+   ;;
+  *) echo "Invalid feature type $feat_type" && exit 1;
+esac
+
+## Set up model and alignment model.
+mdl=$srcdir/final.mdl
+if [ -f $srcdir/final.alimdl ]; then
+  alimdl=$srcdir/final.alimdl
+else
+  alimdl=$srcdir/final.mdl
+fi
+[ ! -f $mdl ] && echo "$0: no such model $mdl" && exit 1;
+alimdl_cmd="gmm-boost-silence --boost=$boost_silence `cat $lang/phones/optional_silence.csl` $alimdl - |"
+mdl_cmd="gmm-boost-silence --boost=$boost_silence `cat $lang/phones/optional_silence.csl` $mdl - |"
+
+
+## Work out where we're getting the graphs from.
+if $use_graphs; then
+  echo "unexpected, see local/align_fmllr_g_pt.sh line 99" && exit 1;
+:<<'END'
+  [ "$nj" != "`cat $srcdir/num_jobs`" ] && \
+    echo "$0: you specified --use-graphs true, but #jobs mismatch." && exit 1;
+  [ ! -f $srcdir/fsts.1.gz ] && echo "No graphs in $srcdir" && exit 1;
+  graphdir=$srcdir
+END
+else
+  graphdir=$dir
+  if [ $stage -le 0 ]; then
+    echo "$0: compiling training graphs"
+    #tra="ark:utils/sym2int.pl --map-oov $oov -f 2- $lang/words.txt $sdata/JOB/text|";   
+    #$cmd JOB=1:$nj $dir/log/compile_graphs.JOB.log  \
+    #  compile-train-graphs $dir/tree $dir/final.mdl  $lang/L.fst "$tra" \
+    #    "ark:|gzip -c >$dir/fsts.JOB.gz" || exit 1;
+
+    #dir_fsts="/export/ws15-pt-data/cliu/data/phonelattices/monophones/engg2p/mandarin_epsilon_3-sym_fixed"
+    #dir_fsts="/export/ws15-pt-data/cliu/data/phonelattices/monophones/trainedp2let/HG_SW_UR_DT_AR_MDdecode-rmeps"
+    $cmd JOB=1:$nj $dir/log/text_pt.JOB.log \
+      cut -f1 "$sdata/JOB/text" \| awk -v dir_fsts=$dir_fsts \
+      '{key=value=$1; value=value".lat.fst"; print key"\t"dir_fsts"/"value}' \
+      \> "$sdata/JOB/text.pt" || exit 1;
+
+      #'{key=value=$1; value=value".saus.fst"; print key"\t"dir_fsts"/"value}' \
+
+    $cmd JOB=1:$nj $dir/log/compile_graphs.JOB.log \
+      compile-train-graphs-fsts-pt --read-disambig-syms=$lang/phones/disambig_new.int \
+      --batch-size=1 --transition-scale=1.0 --self-loop-scale=0.1 $dir/tree $dir/final.mdl  $lang/L_disambig_new.fst  \
+      "ark:cat $sdata/JOB/text.pt|" "ark:|gzip -c >$dir/fsts.JOB.gz" || exit 1;
+      #compile-train-graphs-fsts-pt --read-disambig-syms=$lang/phones/disambig.int \
+      #--batch-size=1 --transition-scale=1.0 --self-loop-scale=0.1 $dir/tree $dir/final.mdl  $lang/L_disambig.fst  \
+  fi
+fi
+
+
+if [ $stage -le 1 ]; then
+  echo "$0: aligning data in $data using $alimdl and speaker-independent features."
+  $cmd JOB=1:$nj $dir/log/align_pass1.JOB.log \
+    gmm-align-compiled $scale_opts --beam=$beam --retry-beam=$retry_beam --careful=$careful "$alimdl_cmd" \
+    "ark:gunzip -c $graphdir/fsts.JOB.gz|" "$sifeats" "ark:|gzip -c >$dir/pre_ali.JOB.gz" || exit 1;
+fi
+
+if [ $stage -le 2 ]; then
+  echo "$0: computing fMLLR transforms"
+  if [ "$alimdl" != "$mdl" ]; then
+    $cmd JOB=1:$nj $dir/log/fmllr.JOB.log \
+      ali-to-post "ark:gunzip -c $dir/pre_ali.JOB.gz|" ark:- \| \
+      weight-silence-post 0.0 $silphonelist $alimdl ark:- ark:- \| \
+      gmm-post-to-gpost $alimdl "$sifeats" ark:- ark:- \| \
+      gmm-est-fmllr-gpost --fmllr-update-type=$fmllr_update_type \
+      --spk2utt=ark:$sdata/JOB/spk2utt $mdl "$sifeats" \
+      ark,s,cs:- ark:$dir/trans.JOB || exit 1;
+  else
+    $cmd JOB=1:$nj $dir/log/fmllr.JOB.log \
+      ali-to-post "ark:gunzip -c $dir/pre_ali.JOB.gz|" ark:- \| \
+      weight-silence-post 0.0 $silphonelist $alimdl ark:- ark:- \| \
+      gmm-est-fmllr --fmllr-update-type=$fmllr_update_type \
+      --spk2utt=ark:$sdata/JOB/spk2utt $mdl "$sifeats" \
+      ark,s,cs:- ark:$dir/trans.JOB || exit 1;
+  fi
+fi
+
+feats="$sifeats transform-feats --utt2spk=ark:$sdata/JOB/utt2spk ark:$dir/trans.JOB ark:- ark:- |"
+
+if [ $stage -le 3 ]; then
+  echo "$0: doing final alignment."
+  #$cmd JOB=1:$nj $dir/log/align_pass2.JOB.log \
+  #  gmm-align-compiled $scale_opts --beam=$beam --retry-beam=$retry_beam --careful=$careful "$mdl_cmd" \
+  #  "ark:gunzip -c $graphdir/fsts.JOB.gz|" "$feats" "ark:|gzip -c >$dir/ali.JOB.gz" || exit 1;
+
+  #maxactive=7000; beam=20.0; lattice_beam=7.0; acwt=0.083333;
+  #maxactive=7000; beam=20.0; lattice_beam=3.0; acwt=0.083333;
+  maxactive=20000; beam=100.0; lattice_beam=3.0; acwt=0.083333;
+  $cmd JOB=1:$nj $dir/log/align_pass2.JOB.log \
+    gmm-latgen-faster --max-active=$maxactive --beam=$beam --lattice-beam=$lattice_beam --acoustic-scale=$acwt \
+      --allow-partial=true --word-symbol-table=$lang/words.txt \
+      $dir/final.mdl "ark:gunzip -c $dir/fsts.JOB.gz|" "$feats" ark:- \| \
+      lattice-to-post --acoustic-scale=$acwt ark:- "ark:|gzip -c >$dir/post.JOB.gz"
+fi
+
+rm $dir/pre_ali.*.gz
+
+echo "$0: done aligning data."
+
+utils/summarize_warnings.pl $dir/log
+
+exit 0;
---
+++ run-pt-text-G-map-2.sh
@@ -0,0 +1,173 @@
+#!/bin/bash -u
+
+# After training an SAT gmm-hmm system with multilingual languages 
+# (among Arabic, Dutch, Mandarin, Hungarian, Swahili, Urdu) of the SBS corpus,
+# proceed to adapt gmm-hmm with probabilistic transcription of target language.
+
+echo `date` && echo $0 
+
+[ -f cmd.sh ] && source ./cmd.sh \
+  || echo "cmd.sh not found. Jobs may not execute properly."
+
+. path.sh || { echo "Cannot source path.sh"; exit 1; }
+
+#---------------------------------------------------------------------------
+# Set the location of the SBS speech 
+SBS_CORPUS=/export/ws15-pt-data/data/audio
+SBS_TRANSCRIPTS=/export/ws15-pt-data/data/transcripts/matched
+SBS_DATA_LISTS=/export/ws15-pt-data/data/lists
+
+# Set the language codes for SBS languages that we will be processing
+#export SBS_LANGUAGES="AR DT MD HG SW UR"
+#export TRAIN_LANG="SW AR UR DT HG"
+#export TEST_LANG="MD"
+export TEST_LANG="SW"
+
+stage=0
+#---------------------------------------------------------------------------
+# generate alignment for training data if needed, e.g., to train a dnn
+if [ $stage -le -1 ]; then
+  mkdir -p exp/tri3b_ali
+	steps/align_fmllr.sh --nj "$train_nj" --cmd "$train_cmd" \
+		data/train data/lang exp/tri3b exp/tri3b_ali || exit 1;
+  echo ------------------------------------------
+fi
+#---------------------------------------------------------------------------
+# Post-process the raw probabilistic lattice/sausages, i.e., pt, 
+# by composing G and pt.
+# On G, add self-loop <#2>:<#2>; on pt, deletion arc is <#2>:<eps>.
+# Maybe need to make corresponding change to this stage to process 
+# different raw pt lattices. Here is an eg.
+
+# add the directory of raw pt, e.g.,
+#dir_raw_pt=/export/ws15-pt-data/data/phonelattices/monophones/trainedp2let/HG_MD_UR_DT_AR_CA_SWdecode
+dir_raw_pt= 
+
+if [ -z $dir_raw_pt ]; then echo "empty dir_raw_pt" && exit 1;fi
+
+dir_fsts=exp/data_pt
+dir_lang=data/$TEST_LANG/lang_test_text_G
+if [ $stage -le 0 ]; then
+  # first, generate G_new.fst
+  # assume $TEST_LANG is the only one held-out language
+  mkdir -p $dir_fsts
+  cp data/$TEST_LANG/local/lm/lm_phone.arpa.gz $dir_fsts/lm_phone.arpa.gz || exit 1;
+
+  gunzip -c $dir_fsts/lm_phone.arpa.gz | egrep -v '<s> <s>|</s> <s>|</s> </s>' | \
+    arpa2fst - | fstprint | utils/eps2disambig.pl | utils/s2eps.pl \
+    > $dir_fsts/lm_phone.txt || exit 1;
+
+  s_bkoff=`cat $dir_fsts/lm_phone.txt | grep "#0" | awk '{print $2}' | uniq` || exit 1;
+  # should be only one backoff state; if not, exit and diagnose
+  num=`echo $s_bkoff | wc -w` || exit 1;
+  if [ $num -gt 1 ]; then echo "expect only one backoff state" && exit 1; fi
+  echo "backoff state $s_bkoff"
+
+  disambig_sym=`tail -n1 $dir_lang/words.txt | awk '{print $2}'` || exit 1;
+  disambig_sym=$((disambig_sym+1))
+  echo "new disambig_sym on G_new.fst " $disambig_sym
+
+  cat $dir_fsts/lm_phone.txt | awk -v disambig=$disambig_sym -v s_bkoff=$s_bkoff 'BEGIN{pre=0;}
+    {if ($1 != pre && pre != s_bkoff && pre != 0) {print pre"\t"pre"\t"disambig"\t"disambig;}
+    print $0; pre=$1;} END{print pre"\t"pre"\t"disambig"\t"disambig}' \
+    > $dir_fsts/G_new_fst || exit 1;
+
+  echo "$disambig_sym $disambig_sym" | cat - $dir_lang/words.txt \
+    > $dir_fsts/words.txt
+
+  cat $dir_fsts/G_new_fst | fstcompile --isymbols=$dir_fsts/words.txt \
+    --osymbols=$dir_fsts/words.txt --keep_isymbols=false --keep_osymbols=false | \
+    fstrmepsilon | fstarcsort --sort_type=ilabel > $dir_fsts/G_new.fst || exit 1;
+  echo ------------------------------------------
+
+  # second, compose G_new.fst with pruned pt lattices, and prune
+  for f in $dir_raw_pt/*lat.fst; do
+    fstprune --weight=2.0 $f | fstprint | awk -v disambig=$disambig_sym \
+      '{if (NF > 3 && $3 == 0) $3 = disambig; print}' | fstcompile | fstarcsort  > $dir_fsts/tmp.fst
+
+    fstcompose $dir_fsts/G_new.fst $dir_fsts/tmp.fst | fstarcsort | fstprune --weight=1.0 > $dir_fsts/${f##/*/}
+
+    rm $dir_fsts/tmp.fst
+    #break
+  done
+  echo ------------------------------------------
+  # third, generate disambig_new.int and L_disambig_new.fst
+  disambig_sym_=`tail -n1 $dir_lang/phones/disambig.int`
+  disambig_sym_=$((disambig_sym_+1))
+  echo "new disambig_sym on L_disambig_new.fst " $disambig_sym_ || exit 0;
+  echo $disambig_sym_ | cat $dir_lang/phones/disambig.int - \
+    > $dir_lang/phones/disambig_new.int || exit 0;
+
+  ##
+  sym_l=`cat $dir_lang/phones.txt | grep "#0" | awk '{print $2}'`
+  sym_g=`cat $dir_lang/words.txt | grep "#0" | awk '{print $2}'`
+  echo "#0 in L and G " $sym_l $sym_g
+  line=`fstprint $dir_lang/L_disambig.fst | grep -P "${sym_l}\t${sym_g}" | \
+    awk -v s_l=$disambig_sym_ -v s_g=$disambig_sym '{$3=s_l; $4=s_g; print}'`
+  echo $line
+  fstprint $dir_lang/L_disambig.fst > $dir_fsts/L_disambig.txt
+  echo $line | cat $dir_fsts/L_disambig.txt - | fstcompile > $dir_lang/L_disambig_new.fst || exit 1;
+
+fi
+#echo `date` && exit 0
+#---------------------------------------------------------------------------
+feats_nj=4
+train_nj=8
+decode_nj=4
+
+# Adapting SAT+LDA+MLLT triphone systems
+for L in $TEST_LANG; do
+  alidir_g_pt=exp/tri3b_ali_${L}_g_pt_text_G
+  if [ $stage -le 1 ]; then
+    # align pt of target language
+    # $dir_fsts needs to be properly assigned and contains the processed pt
+    if [ -z $dir_fsts ]; then echo "empty $dir_fsts" && exit 1;fi
+
+    echo "Aligning PTs"
+
+    mkdir -p $alidir_g_pt
+    local/align_fmllr_g_pt.sh --nj "$train_nj" --cmd "$train_cmd" data/$L/train \
+      $dir_lang exp/tri3b $alidir_g_pt $dir_fsts || exit 1;
+    echo ------------------------------------------
+  fi
+
+  exp_dir=exp/tri3b_map_${L}_g_pt_text_G
+  if [ $stage -le 2 ]; then
+    echo "Adapting to PTs"
+
+    #local/train_sat_map_pt.sh \
+    local/train_sat_map_pt.sh --cmd "$train_cmd" \
+      data/$L/train $dir_lang $alidir_g_pt $exp_dir || exit 1;
+    echo ------------------------------------------
+  fi
+
+  if [ $stage -le 3 ]; then
+    echo "Decoding"
+
+    graph_dir=$exp_dir/graph_text_G
+    mkdir -p $graph_dir
+    utils/mkgraph.sh $dir_lang $exp_dir $graph_dir || exit 1;
+    echo ------------------------------------------
+
+    dataset=dev
+    #dataset=eval
+    #steps/decode_fmllr.sh --nj "$decode_nj" --max_active 20000 --lattice_beam 4.0 $graph_dir \
+    steps/decode_fmllr.sh --nj "$decode_nj" --cmd "$decode_cmd" $graph_dir \
+      data/$dataset $exp_dir/decode_$dataset || exit 1;
+    echo ------------------------------------------
+
+    grep WER $exp_dir/decode_${dataset}.si/wer_* | sort -nk2 | head -n2
+    echo -------------------------------
+    grep WER $exp_dir/decode_${dataset}/wer_* | sort -nk2 | head -n2
+    echo -------------------------------
+    echo `date`
+  fi
+done
+
+
+
+#---------------------------------------------------------------------------
+# Getting PER numbers
+# for x in exp/*/*/decode*; do [ -d $x ] && grep WER $x/wer_* | utils/best_wer.sh; done
+
+
